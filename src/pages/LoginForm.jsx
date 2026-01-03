b31e4cc3772f64156d7bee9a0a58bd12fe8814d4
typkers <katmic4@gmail.com>
2026-01-03 17:06:40 +0100

    Added logo, create new group UI

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,3 +1,5 @@
+import "../styles/login.css";
+import logo from "../assets/logo.png";
 import { useState } from "react";
 
 export default function Login({ goToRegister, onLogin }) {
@@ -19,50 +21,54 @@ export default function Login({ goToRegister, onLogin }) {
     }
 
     return (
-        <div className="page">
-            <div className="card">
-                <div className="logo">logoPic</div>
+        <div className="auth login">
+            <div className="page">
+                <div className="card">
+                    <div className="logo">
+                        <img src={logo} alt="Divi logo" />
+                    </div>
 
-                <h1 className="title">DIVI</h1>
-                <p className="subtitle">Welcome to your Expense Sharing Hub!</p>
+                    <h1 className="title">DIVI</h1>
+                    <p className="subtitle">Welcome to your Expense Sharing Hub!</p>
 
-                <div className="tabs">
-                    <button type="button" className="tab active">
-                        Login
-                    </button>
+                    <div className="tabs">
+                        <button type="button" className="tab active">
+                            Login
+                        </button>
 
-                    <button type="button" className="tab" onClick={goToRegister}>
-                        Register
-                    </button> 
+                        <button type="button" className="tab" onClick={goToRegister}>
+                            Register
+                        </button> 
+                    </div>
+
+                    <form className="form" onSubmit={handleSubmit}>
+                        <div className="field">
+                            <label>Email address</label>
+                            <input 
+                                type="email"
+                                placeholder="test@divi.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="field">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                placeholder="********"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <button className="primary" type="submit" >
+                            Sign in
+                        </button>
+                    </form>
                 </div>
-
-                <form className="form" onSubmit={handleSubmit}>
-                    <div className="field">
-                        <label>Email address</label>
-                        <input 
-                            type="email"
-                            placeholder="test@divi.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <div className="field">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            placeholder="********"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <button className="primary" type="submit" >
-                        Sign in
-                    </button>
-                </form>
             </div>
         </div>
     )
--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,3 +1,5 @@
+import "../styles/register.css";
+import logo from "../assets/logo.png";
 import {useState } from "react";
 
 export default function Register( { goToLogin}) {
@@ -15,78 +17,82 @@ export default function Register( { goToLogin}) {
         return;
         }
 
-        alert(`Registered (mock)\nName: ${name}\nEmail: ${email}`);
+        alert(`Registered successfully!`);
 
         goToLogin();
     }
 
     return (
-        <div className="page">
-            <div className="card">
-                <div className="logo">logoPic</div>
+        <div className="auth register">
+            <div className="page">
+                <div className="card">
+                    <div className="logo">
+                        <img src={logo} alt="Divi logo" />
+                    </div>
 
-                <h1 className="title">DIVI</h1>
-                <p className="subtitle">Welcome to your Expense Sharing Hub!</p>
+                    <h1 className="title">DIVI</h1>
+                    <p className="subtitle">Welcome to your Expense Sharing Hub!</p>
 
-                <div className="tabs">
-                    <button type="button" className="tab" onClick={goToLogin}>
-                        Login
-                    </button>
+                    <div className="tabs">
+                        <button type="button" className="tab" onClick={goToLogin}>
+                            Login
+                        </button>
 
-                    <button type="button" className="tab active">
-                        Register
-                    </button>
+                        <button type="button" className="tab active">
+                            Register
+                        </button>
+                    </div>
+
+                    <form className="form" onSubmit={handleSubmit}>
+                        <div className="field">
+                            <label>Name</label>
+                            <input
+                                placeholder="Your name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                required
+
+                            />
+                        </div>
+
+                        <div className="field">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                placeholder="your@email.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="field">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                placeholder="******"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="field">
+                            <label>Confirm password</label>
+                            <input
+                                type="password"
+                                placeholder="******"
+                                value={confirm}
+                                onChange={(e) => setConfirm(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <button className="primary" type="submit">
+                            Register
+                        </button>
+                    </form>
                 </div>
-
-                <form className="form" onSubmit={handleSubmit}>
-                    <div className="field">
-                        <label>Name</label>
-                        <input
-                            placeholder="Your name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-
-                        />
-                    </div>
-
-                    <div className="field">
-                        <label>Email address</label>
-                        <input
-                            type="email"
-                            placeholder="your@email.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <div className="field">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            placeholder="******"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <div className="field">
-                        <label>Confirm password</label>
-                        <input
-                            type="password"
-                            placeholder="******"
-                            value={confirm}
-                            onChange={(e) => setConfirm(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <button className="primary" type="submit">
-                        Register
-                    </button>
-                </form>
             </div>
         </div>
     )
