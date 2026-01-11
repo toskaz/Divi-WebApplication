efb3a41760ba4b59785c0473d2a8fbfc93d515c5
Mateusz Stankiewicz <matistan05@gmail.com>
2026-01-12 00:07:24 +0100

    fixed some git files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../backend/src/main/resources/static',
-    emptyOutDir: true
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
   }
 })
