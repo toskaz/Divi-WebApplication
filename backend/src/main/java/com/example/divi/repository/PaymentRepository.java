8a6e7738def17232f21544cafd7854e785f96922
Mateusz Balicki <280293@student.pwr.edu.pl>
2026-01-13 13:03:01 +0100

    PaymentRepository update

--- backend/src/main/java/com/example/divi/repository/PaymentRepository.java
+++ backend/src/main/java/com/example/divi/repository/PaymentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PaymentReposiotry extends JpaRepository<Payment,Long> {
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
     List<Payment> findByGroupOrderByDateDesc(Group group);
     List<Payment> findByGroup_GroupIdOrderByDateDesc(Long groupId);
     List<Payment> findByUser_UserId(Long userId);
